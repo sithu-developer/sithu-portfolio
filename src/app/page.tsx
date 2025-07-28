8e13ba60c8352a455e4e196adb06997a45e793a2
unknown <sithunaing8209@gmail.com>
2025-07-29 01:31:52 +0630

    profile animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import About from "@/components/About";
+import About2 from "@/components/About2";
 import Projects from "@/components/Projects";
 import TopBar from "@/components/TopBar";
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box sx={ { position : "relative" , display : "flex" , flexDirection : "column" , justifyContent : "center" , alignItems : "center" , minHeight : "100vh" , width : "100vw" , overflowY : 'auto' , backgroundImage : "url('/darkBg.jpg')" , backgroundRepeat : "no-repeat" , backgroundPosition : "center" , backgroundAttachment : "fixed" , backgroundSize : "cover"}}>
+    <Box sx={ { position : "relative" , display : "flex" , flexDirection : "column" , justifyContent : "center" , alignItems : "center" , minHeight : "100vh" , width : "100vw" , overflowY : 'auto' , backgroundImage : "url('/pc.png')" , backgroundRepeat : "no-repeat" , backgroundPosition : "center" , backgroundAttachment : "fixed" , backgroundSize : "cover"}}>
       <TopBar />
-      <About />
+      <About2 />
       <Projects />
     </Box>
   );
--- src/components/About2.tsx
+++ src/components/About2.tsx
@@ -3,8 +3,10 @@
 import { Box, Slide, Typography } from "@mui/material";
 import Image from "next/image";
 import { useRef } from "react";
+import "@/style/about.css";
 
-const About = () => {
+
+const About2 = () => {
     const containerRef = useRef<HTMLElement>(null);
 
     return (
@@ -16,18 +18,14 @@ const About = () => {
                 <Typography variant="h5" sx={{ textAlign : "center"}} >I love turning complex problems into elegant solutions. As a Next.js full-stack developer, I enjoy the entire development lifecycle - from architecting data models to deploying serverless functions and creating intuitive front-end interfaces.  I'm always looking for new challenges that allow me to grow my skills and contribute to meaningful projects.</Typography>
             </Box>
           </Slide>
-          <Box sx={{ position : "relative" , display : "flex" , justifyContent : "center" , alignItems : "center"}}>
-            <Box ref={containerRef} sx={{ width : "450px" , height : "500px" , borderRadius : "50px" , overflow : "hidden" , display : "flex" , justifyContent : "center" , alignItems : "start" , backgroundColor: "rgba(255, 255, 255, 0.1)", backdropFilter :  "blur(10px)", border: "1px solid rgba(255, 255, 255, 0.3)" , boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)" }}>
-              <Slide in direction="up" container={containerRef.current} timeout={{ enter : 2000 }} >
-                  <Image alt="Sithu Naing's photo" src={"/sithu-photo.png"} width={1000} height={1000} style={{ width : "550px" , height : "auto"}}  />
-              </Slide>
-            </Box>
-            <Box sx={{ position : "absolute" , width : "100%" , height : "100%" , opacity : 0 , transition : "all 0.5s ease,opacity 0.2s linear" , ":hover" : { transform : "translateX(-200px)" , opacity : 1 } }}>
-              <Image alt="panda" src={"/panda-hi.png"} width={400} height={400} style={{ width : "200px" , height : "auto"}} />
+          <Box className="profile-container" >
+            <Box  className="profile-wrapper" >
+                <Image className="profile-img" alt="Sithu Naing's photo" src={"/sithu-photo.png"} width={1000} height={1000} style={{ width : "450px" , height : "auto"}}  />
+                <Box className="glow-effect"></Box>
             </Box>
           </Box>
         </Box>
     )
 }
 
-export default About;
+export default About2;
