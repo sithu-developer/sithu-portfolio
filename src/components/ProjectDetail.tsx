206f23e75b7c45223a26aae928add218f2d340d1
unknown <sithunaing8209@gmail.com>
2025-08-22 00:41:50 +0630

    DV detail admin done

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -21,7 +21,7 @@ const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) =
                     </IconButton>
                     <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" }, fontWeight : 600 , background : "linear-gradient(to right, #dde1e4ff , #2196f3, #21cbf3 , white)" , backgroundClip : "text" , WebkitBackgroundClip : "text" , WebkitTextFillColor : "transparent" }} >{projectDetailItems.id === 1 ? "Happy Chatting" : "Digital Voting System"}</Typography>
                 </Box>
-                <Box sx={{ width : "100%" , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : "row"} , justifyContent : "center" , alignItems : "center" , gap : { xs : "20px" , sm :  "60px"} , pt : { xs : "50px" , sm : "0px"}}}>
+                <Box sx={{ width : "100%" , minHeight : { xs : "95%" , sm :  "80%" } , display : "flex" , flexDirection : { xs : "column" , sm : "row"} , justifyContent : "center" , alignItems : "center" , gap : { xs : "20px" , sm :  "60px"} }}>
                     <Box sx={{ width : { xs : "100px" , sm : "160px"} , height : { xs : "200px" , sm : "300px" }}}>
                         <Image  alt={"hc"} src={(projectDetailItems.id === 1 ? "/hc-phone.png" : "/digital-voting-sign-in.png")} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px"  }} />
                     </Box>
@@ -63,42 +63,43 @@ const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) =
                         </Box>
                     </Box>
                 </Box>
-                {projectDetailItems.id === 1 ? (happyChattingDetails.map(item => (
-                    <Box key={item.id} sx={{ width : "100%"  , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row" : "row-reverse") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
-                        <Box sx={{ width : { xs : "350px" , sm : "600px" } , height : { xs : "220px" , sm : "400px" }}}>
-                            <Image  alt={"happy-chatting-detail-img"} src={item.imgUrl} width={1000} height={1000} style={{  width : "100%" , height : "auto" , borderRadius : "5px" }} />
+                    {projectDetailItems.id === 1 ? (happyChattingDetails.map(item => (
+                        <Box key={item.id} sx={{ width : "100%"  , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row" : "row-reverse") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
+                            <Box sx={{ width : { xs : "350px" , sm : "600px" } , height : { xs : "220px" , sm : "400px" }}}>
+                                <Image  alt={"happy-chatting-detail-img"} src={item.imgUrl} width={1000} height={1000} style={{  width : "100%" , height : "auto" , borderRadius : "5px" }} />
+                            </Box>
+                            <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px", maxWidth : "550px" }}>
+                                <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
+                                {item.messages.map(mess => (
+                                    <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
+                                        <CircleIcon sx={{ fontSize : 8 , mt : "9px" }} />
+                                        <Typography>{mess}</Typography>
+                                    </Box>
+                                ))}
+                            </Box>
                         </Box>
-                        <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px" }}>
-                            <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
-                            {item.messages.map(mess => (
-                                <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
-                                    <CircleIcon sx={{ fontSize : 8 , mt : "9px" }} />
-                                    <Typography>{mess}</Typography>
-                                </Box>
-                            ))}
+                    )))
+                    : (digitalVotingDetails.map(item => (
+                        <Box key={item.id} sx={{ width : "100%"  , minHeight : { xs : "95%" , sm :  "80%" }, display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row-reverse" : "row") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
+                            <Box sx={{ display : "flex" , gap : "10px"}}>
+                                {item.imgUrls.map(url => (
+                                    <Box key={url} sx={{ width : { xs : "105px" , sm : "185px"} , height : { xs : "200px" , sm : "350px" } }}>
+                                        <Image  alt={"digital-voting-detail-img"} src={url} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px" }} />
+                                    </Box>
+                                ))}
+                            </Box>
+                            <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px" , maxWidth : "550px" }}>
+                                <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
+                                {item.messages.map(mess => (
+                                    <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
+                                        <CircleIcon sx={{ fontSize : 8 , mt : "9px" }}  />
+                                        <Typography>{mess}</Typography>
+                                    </Box>
+                                ))}
+                            </Box>
                         </Box>
-                    </Box>
-                )))
-                : (digitalVotingDetails.map(item => (
-                    <Box key={item.id} sx={{ width : "100%"  , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row-reverse" : "row") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
-                        <Box sx={{ display : "flex" , gap : "10px"}}>
-                            {item.imgUrls.map(url => (
-                                <Box key={url} sx={{ width : { xs : "105px" , sm : "185px"} , height : { xs : "200px" , sm : "350px" } }}>
-                                    <Image  alt={"digital-voting-detail-img"} src={url} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px" }} />
-                                </Box>
-                            ))}
-                        </Box>
-                        <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px" }}>
-                            <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
-                            {item.messages.map(mess => (
-                                <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
-                                    <CircleIcon sx={{ fontSize : 8 , mt : "9px" }}  />
-                                    <Typography>{mess}</Typography>
-                                </Box>
-                            ))}
-                        </Box>
-                    </Box>
-                )))}
+                    )))}
+                {/* </Box> */}
             </DialogContent>
         </Dialog>
     )
@@ -159,24 +160,42 @@ const digitalVotingDetails : DigitalVotingDetailType[] = [
         id : 1,
         heading : "Admin Sign Up",
         imgUrls : [ "/digital-voting-sign-in.png" , "/digital-voting-admin-sign.png" , "/king-queen-page.png"  ],
-        messages : [ "Authentication is running with Google" ,  "To sign up your gmail as an admin , you need to know the Admin code" , "The number of admin accounts is also limited" , "Admin code and limit can be changed by Admin in setting" , "When you have done signing up as an admin , you will reach to the third photo " ]
+        messages : [ "Authentication is running with Google" ,  "To become an admin, a user must sign in with their Gmail account and enter a specific admin code" , "For security purposes, the number of admin accounts is limited" , "The admin code and the maximum number of admin accounts can be modified by an existing administrator in the settings" , "Once a user successfully signs up as an admin, they are granted access to the features shown in the third photo" ]
     },
     {
         id : 2,
-        heading : "Categories CRUD",
+        heading : "Category Management",
         imgUrls : [ "/category-create.png" , "/category-update.png" , "/category-delete.png" ],
-        messages : [ "In this page, an admin can create new category like 'Miss' in photo with crown-logo photo" , "In the second photo, you can rename only category's name or change photo, or both" , "The third photo shows deleting  category, but it can be done with the correct admin code" ]
+        messages : [ "The first image shows the interface for creating a new category, such as \"Miss\", complete with the ability to upload a corresponding photo, like a crown logo." , "The second image demonstrates the editing functionality, where an administrator can rename a category, change its photo, or update both the name and the photo simultaneously." , "Finally, the third image illustrates the process of deleting a category. To ensure security and prevent accidental removal, this action requires a unique and valid administrator code to be entered before the deletion can be completed." ]
+    },
+    {
+        id : 3,
+        heading : "Contestant Management",
+        imgUrls : [ "/student-create.png" , "/student-after-create.png" , "/student-update-delete.png" ],
+        messages : [ "In the same page, a new contestant can be created for the selected category" , "Each contestant is assigned a unique number to ensure there are no duplicates within that category" , "To update or delete a contestant, simply click on them to access their profile and make changes." ]
+    },
+    {
+        id : 4,
+        heading : "Agenda Management",
+        imgUrls : [ "/agenda-page.png" , "/agenda-create.png" , "/agenda-update-delete.png"],
+        messages : [ "The second icon on the sidebar provides access to the agenda page" , "You can create a new agenda simply by uploading a photo" ,  "Once an agenda photo is uploaded, you have the flexibility to download, replace, or delete it as needed" ]
+    },
+    {
+        id : 5,
+        heading : "Votes & Announce",
+        imgUrls : [ "/votes-count-page.png" , "/votes-list.png" , "/votes-announcement.png"],
+        messages : [ "The third icon on the sidebar will lead to the Votes and Announce page" , "In this page, you can monitor the real-time voting results for each contestant" , "To reveal the winners, simply press the \"Show Result\" button, which will announce the winner of each category" , "For security purposes, announcement can only be done with the valid administrator code"  ]
+    },
+    {
+        id : 6,
+        heading : "Setting",
+        imgUrls : [ "/setting-page.png" , "/setting-admin-code.png" , "/setting-major-code.png"],
+        messages : [ "The final icon on the sidebar will take to the setting page" , "In this page, the admin code and the number of administrator accounts can be changed" , "Additionally, this page allows you to create, update, or delete major codes for voters" , "To close voting from voters , simply press the \"Close Voting\" button" , "For security, all of these actions require a valid admin code to be entered before they can be executed" , "To sign out of the administrator account, simply press the sign-out button"]
     },
     // {
-    //     id : 3,
+    //     id : 7,
     //     heading : "",
-    //     imgUrl : [],
-    //     messages : [ ]
-    // },
-    // {
-    //     id : 4,
-    //     heading : "",
-    //     imgUrl : [],
+    //     imgUrls : [ "/" , "/" , "/"],
     //     messages : [ ]
     // },
     
