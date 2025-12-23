973ce534fcc3e2a1f302702c1162a08ff4831e51
unknown <sithunaing8209@gmail.com>
2025-12-23 11:35:38 +0630

    project detail done

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -12,24 +12,27 @@ interface Props {
 }
 
 const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) => {
+
+    if(!projectDetailItems.selectedProject) return null;
+
     return (
         <Dialog open={projectDetailItems.open} fullScreen >
             <DialogContent sx={{ backgroundImage : "url('/lp.png')" , backgroundRepeat : "no-repeat" , backgroundPosition : "center" , backgroundAttachment : "fixed" , backgroundSize : "cover"}}>
                 <Box  sx={{ display : "flex" , justifyContent : "start", alignItems : "center" , gap : "20px" , py : "10px" , px : { xs : "10px" , sm : "30px"} , width : "100vw" , position : "fixed" , top : 0 , left : 0 , zIndex : 100  , backgroundColor: "rgba(64, 60, 60, 0.1)", backdropFilter :  "blur(10px)", }}>
-                    <IconButton color="secondary" onClick={() => setProjectDetailItems({id : 0 , open : false})} >
+                    <IconButton color="secondary" onClick={() => setProjectDetailItems({selectedProject : null , open : false})} >
                         <CloseRoundedIcon sx={{ color : "white"}} />
                     </IconButton>
-                    <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" }, fontWeight : 600 , background : "linear-gradient(to right, #dde1e4ff , #2196f3, #21cbf3 , white)" , backgroundClip : "text" , WebkitBackgroundClip : "text" , WebkitTextFillColor : "transparent" }} >{projectDetailItems.id === 1 ? "Happy Chatting" : "Digital Voting System"}</Typography>
+                    <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" }, fontWeight : 600 , background : "linear-gradient(to right, #dde1e4ff , #2196f3, #21cbf3 , white)" , backgroundClip : "text" , WebkitBackgroundClip : "text" , WebkitTextFillColor : "transparent" }} >{projectDetailItems.selectedProject.name}</Typography>
                 </Box>
                 <Box sx={{ width : "100%" , minHeight : { xs : "95%" , sm :  "80%" } , display : "flex" , flexDirection : { xs : "column" , sm : "row"} , justifyContent : "center" , alignItems : "center" , gap : { xs : "10px" , sm :  "60px"} , pt : { xs : "50px" , sm : "0px"} }}>
                     <Box sx={{ width : { xs : "100px" , sm : "160px"} , height : { xs : "200px" , sm : "300px" }}}>
-                        <Image  alt={"hc"} src={(projectDetailItems.id === 1 ? "/hc-phone.png" : "/digital-voting-sign-in.png")} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px"  }} />
+                        <Image  alt={"hc"} src={projectDetailItems.selectedProject.headingUrl} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px"  }} />
                     </Box>
                     <Box sx={{ display : "flex" , flexDirection : "column" , gap : { xs : "10px" , sm : "30px" } , width : { xs : "340px" , sm : "700px"} }}>
-                        <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 , textAlign : { xs : "center" , sm : "start" } }}>{(projectDetailItems.id === 1 ? "Happy Chatting" : "Digital Voting System")}</Typography>
+                        <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 , textAlign : { xs : "center" , sm : "start" } }}>{projectDetailItems.selectedProject.name}</Typography>
                         <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px"}}>
-                            <Typography sx={{ fontSize : "17px"}}>{(projectDetailItems.id === 1 ? "A straightforward messaging platform that strips away the noise, focusing purely on what matters: the conversation." : "Developed a secure and user-friendly digital voting application for university events, such as a \"Fresher Welcome\" celebration.")}</Typography>
-                            {(projectDetailItems.id === 1 ? happyChattingHeadingDetails : digitalVotingHeadingDetails ).map(item => (
+                            <Typography sx={{ fontSize : "17px"}}>{projectDetailItems.selectedProject.headingTypo}</Typography>
+                            {(projectDetailItems.selectedProject.headingDetail).map(item => (
                                 <Box key={item.id} sx={{ display : "flex" , gap : "10px" }}>
                                     <CircleIcon sx={{ fontSize : 8 , color : "#00bcd4" , mt : "9px"}} />
                                     <Typography>  
@@ -40,39 +43,24 @@ const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) =
                             ))}
                         </Box>
                         <Box sx={{ display : "flex" , alignItems : "center" , gap : "15px" , mt : { xs : "10px" , sm : "0px"} }}>
-                            <Button component="a" href={(projectDetailItems.id === 1 ? "https://chatting-rust.vercel.app" : "https://digital-voting.vercel.app")} target="_blank" rel="noopener noreferrer" variant="outlined" sx={{ display :"flex" , alignItems : "center" , gap : "10px" , textTransform : "none" , boxShadow : "0px 0px 5px #2BCFFC" , transition : "all ease" , ":hover" : { boxShadow : "3px 3px 5px #2BCFFC"} }}>
+                            <Button component="a" href={(projectDetailItems.selectedProject.liveDemo)} target="_blank" rel="noopener noreferrer" variant="outlined" sx={{ display :"flex" , alignItems : "center" , gap : "10px" , textTransform : "none" , boxShadow : "0px 0px 5px #2BCFFC" , transition : "all ease" , ":hover" : { boxShadow : "3px 3px 5px #2BCFFC"} }}>
                                 <OpenInNewRoundedIcon sx={{ color : "#2BCFFC" , fontSize : "30px"}} />
                                 <Typography sx={{ color : "#2BCFFC"}} >Live Demo</Typography>
                             </Button>
-                            <Button component="a" href={(projectDetailItems.id === 1 ? "https://github.com/sithu-developer/chatting.git" : "https://github.com/sithu-developer/Digital-Voting.git")} target="_blank" rel="noopener noreferrer" variant="outlined" sx={{ display :"flex" , alignItems : "center" , gap : "10px" , textTransform : "none" , boxShadow : "0px 0px 5px #2BCFFC" , transition : "all ease" , ":hover" : { boxShadow : "3px 3px 5px #2BCFFC"} }}>
+                            <Button component="a" href={(projectDetailItems.selectedProject.codeSource)} target="_blank" rel="noopener noreferrer" variant="outlined" sx={{ display :"flex" , alignItems : "center" , gap : "10px" , textTransform : "none" , boxShadow : "0px 0px 5px #2BCFFC" , transition : "all ease" , ":hover" : { boxShadow : "3px 3px 5px #2BCFFC"} }}>
                                 <Image alt="github icon" src={"/github-icon.svg"} width={500} height={500} style={{ width : "30px" , height : "auto" }} />
                                 <Typography sx={{ color : "#2BCFFC"  }} >Code</Typography>
                             </Button>
                         </Box>
                     </Box>
                 </Box>
-                {projectDetailItems.id === 1 ? (happyChattingDetails.map(item => (
-                    <Box key={item.id} sx={{ width : "100%"  , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row" : "row-reverse") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
-                        <Box sx={{ width : { xs : "350px" , sm : "600px" } , height : { xs : "220px" , sm : "400px" }}}>
-                            <Image  alt={"happy-chatting-detail-img"} src={item.imgUrl} width={1000} height={1000} style={{  width : "100%" , height : "auto" , borderRadius : "5px" }} />
-                        </Box>
-                        <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px", maxWidth : "550px" }}>
-                            <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
-                            {item.messages.map(mess => (
-                                <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
-                                    <CircleIcon sx={{ fontSize : 8 , mt : "9px" }} />
-                                    <Typography>{mess}</Typography>
-                                </Box>
-                            ))}
-                        </Box>
-                    </Box>
-                )))
-                : (digitalVotingDetails.map(item => (
+
+                {projectDetailItems.selectedProject.bodyDetail.map(item => (
                     <Box key={item.id} sx={{ width : "100%"  , minHeight : { xs : "95%" , sm :  "80%" }, display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row-reverse" : "row") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
                         <Box sx={{ display : "flex" , gap : "10px"}}>
-                            {item.imgUrls.map(url => (
-                                <Box key={url} sx={{ width : { xs : "105px" , sm : "185px"} , height : { xs : "200px" , sm : "350px" } }}>
-                                    <Image  alt={"digital-voting-detail-img"} src={url} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px" }} />
+                            {item.imgUrls.map(url => ( 
+                                <Box key={url} sx={{ width : "auto" , height : { xs : "200px" , sm : "350px" } }}> {/* here */}
+                                    <Image  alt={item.heading} src={url} width={1000} height={1000} style={{  width : "auto" , height : "100%" , borderRadius : "5px" }} />
                                 </Box>
                             ))}
                         </Box>
@@ -86,7 +74,7 @@ const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) =
                             ))}
                         </Box>
                     </Box>
-                )))}
+                ))}
             </DialogContent>
         </Dialog>
     )
@@ -94,13 +82,54 @@ const ProjectDetail = ({ projectDetailItems , setProjectDetailItems } : Props) =
 
 export default ProjectDetail;
 
-interface HeadingDetail {
+// projectDetailItems.id === 1 ? (happyChattingDetails.map(item => (
+//     <Box key={item.id} sx={{ width : "100%"  , minHeight : "80%" , display : "flex" , flexDirection : { xs : "column" , sm : (item.id % 2 ? "row" : "row-reverse") } , justifyContent : "center" , alignItems : "center" , gap : { xs : "30px" , sm : "60px" }}}>
+//         <Box sx={{ width : { xs : "350px" , sm : "600px" } , height : { xs : "220px" , sm : "400px" }}}>
+//             <Image  alt={"happy-chatting-detail-img"} src={item.imgUrl} width={1000} height={1000} style={{  width : "100%" , height : "auto" , borderRadius : "5px" }} />
+//         </Box>
+//         <Box sx={{ display : "flex" , flexDirection : "column" , gap : "10px", maxWidth : "550px" }}>
+//             <Typography sx={{ fontSize : { xs : "30px" , sm : "35px" } , color : "#00bcd4" , fontWeight : 600 }}>{item.heading}</Typography>
+//             {item.messages.map(mess => (
+//                 <Box key={mess} sx={{ display : "flex" , gap : "10px"}}>
+//                     <CircleIcon sx={{ fontSize : 8 , mt : "9px" }} />
+//                     <Typography>{mess}</Typography>
+//                 </Box>
+//             ))}
+//         </Box>
+//     </Box>
+// )))
+// : 
+
+export interface HeadingDetail {
     id : number;
     heading : string;
     about : string;
 }
 
-const digitalVotingHeadingDetails : HeadingDetail[] = [
+export const malaMaocaiHeadingDetails : HeadingDetail[] = [
+    {
+        id : 1,
+        heading : "About ",
+        about : "- Platform that is bridging the gap between hungry customers and efficient kitchen management."
+    },
+    {
+        id : 2 ,
+        heading : "User-Friendly Design ",
+        about :"- The application's design prioritizes a simple and intuitive user experience for both kitchen and customers"  // here
+    },
+    {
+        id : 3 ,
+        heading : "My Role ",
+        about :"- As the sole developer, I was responsible for the entire full-stack development of this application"
+    },
+    {
+        id : 4 ,
+        heading : "Modern Tech Stack ",
+        about :"- Powered by Next.js(App Router), TypeScript, Tailwind CSS, NextAuth.js, Prisma ORM,Redux Toolkit, MongoDB, Vercel, and MongoDB Atlas"
+    },
+]
+
+export const digitalVotingHeadingDetails : HeadingDetail[] = [
     {
         id : 1 ,
         heading : "About ",
@@ -119,12 +148,12 @@ const digitalVotingHeadingDetails : HeadingDetail[] = [
     {
         id : 4 ,
         heading : "Modern Tech Stack ",
-        about :"- Powered by Next.js, TypeScript, Material UI (MUI), NextAuth.js, Prisma ORM,Redux Toolkit, PostgreSQL, Vercel, and Render for PostgreSQL"
+        about :"- Powered by Next.js, TypeScript, Material UI (MUI), NextAuth.js, Prisma ORM,Redux Toolkit, PostgreSQL, Vercel, and Render for managing PostgreSQL"
     },
    
 ]
 
-const happyChattingHeadingDetails : HeadingDetail[] = [
+export const happyChattingHeadingDetails : HeadingDetail[] = [
     {
         id : 1 ,
         heading : "About",
@@ -143,59 +172,54 @@ const happyChattingHeadingDetails : HeadingDetail[] = [
     {
         id : 4 ,
         heading : "Modern Tech Stack ",
-        about :"- Powered by Next.js, TypeScript, Material UI (MUI), NextAuth.js, Prisma ORM,Redux Toolkit, PostgreSQL, Vercel, and Aiven for PostgreSQL"
+        about :"- Powered by Next.js, TypeScript, Material UI (MUI), NextAuth.js, Prisma ORM,Redux Toolkit, PostgreSQL, Vercel, and Aiven for managing PostgreSQL"
     },
     
 ]
 
-interface HappyChattingDetailType {
-    id : number,
-    imgUrl : string,
-    heading : string,
-    messages : string[]
-}
 
-const happyChattingDetails : HappyChattingDetailType[] = [
-    {
-        id : 0,
-        imgUrl : "/happy-chatting-laptop.png",
-        heading : "Sign In",
-        messages : [ "Users can sign in easily and securely with their Google account" , "I integrated NextAuth to provide secure and effortless sign-in with Google accounts"]
-    },
-    {
-        id : 1,
-        imgUrl : "/hc-main.png",
-        heading : "Main Page",
-        messages : [ "In main page , user can see his chats with his friends and saved-messages" , "There are also menu icon and search icon in top-bar"]
-    },
-    {
-        id : 2,
-        imgUrl : "/hc-sidebar.png",
-        heading : "In Side Bar",
-        messages : [ "It offers simple UI to edit Profile , explore new friends and open Saved Messages" , "Users can sign out in Settings" ]
-    },
-    {
-        id : 3,
-        imgUrl : "/hc-search.png",
-        heading : "Search",
-        messages : [ "It offers easily , simple and efficient interface for searching" , "User can search both friends and messages at the same time"]
-    },
-    {
-        id : 4,
-        imgUrl : "/hc-message.png",
-        heading : "In chat",
-        messages : [ "Users can send messages , photos , emoji and voice" , "Users can reply , copy , forward , edit and delete the messages" , "And can also pin the messages to the top of the chat"]
-    },
-];
-
-interface DigitalVotingDetailType {
+export interface BodyDetailType {
     id : number,
     imgUrls : string[],
     heading : string,
     messages : string[]
 }
 
-const digitalVotingDetails : DigitalVotingDetailType[] = [
+export const happyChattingDetails : BodyDetailType[] = [
+    {
+        id : 0,
+        imgUrls : ["/happy-chatting-laptop.png"],
+        heading : "Sign In",
+        messages : [ "Users can sign in easily and securely with their Google account" , "I integrated NextAuth to provide secure and effortless sign-in with Google accounts"]
+    },
+    {
+        id : 1,
+        imgUrls : ["/hc-main.png"],
+        heading : "Main Page",
+        messages : [ "In main page , user can see his chats with his friends and saved-messages" , "There are also menu icon and search icon in top-bar"]
+    },
+    {
+        id : 2,
+        imgUrls : ["/hc-sidebar.png"],
+        heading : "In Side Bar",
+        messages : [ "It offers simple UI to edit Profile , explore new friends and open Saved Messages" , "Users can sign out in Settings" ]
+    },
+    {
+        id : 3,
+        imgUrls : ["/hc-search.png"],
+        heading : "Search",
+        messages : [ "It offers easily , simple and efficient interface for searching" , "User can search both friends and messages at the same time"]
+    },
+    {
+        id : 4,
+        imgUrls : ["/hc-message.png"],
+        heading : "In chat",
+        messages : [ "Users can send messages , photos , emoji and voice" , "Users can reply , copy , forward , edit and delete the messages" , "And can also pin the messages to the top of the chat"]
+    },
+];
+
+
+export const digitalVotingDetails : BodyDetailType[] = [
     {
         id : 1,
         heading : "Admin Sign Up",
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,17 +3,17 @@ import { useEffect, useRef, useState } from "react";
 import { Box, Grow, Typography } from "@mui/material";
 import Image from "next/image";
 import "@/style/project.css";
-import ProjectDetail from "./ProjectDetail";
+import ProjectDetail, { BodyDetailType, digitalVotingDetails, digitalVotingHeadingDetails, happyChattingDetails, happyChattingHeadingDetails, HeadingDetail, malaMaocaiHeadingDetails } from "./ProjectDetail";
 
 export interface ProjectDetailItems { 
   open : boolean, 
-  id : number 
+  selectedProject : ProjectType | null;
 }
 
 const Projects = () => {
   const [visible, setVisible] = useState(false);
   const boxRef = useRef<HTMLDivElement>(null);
-  const [ projectDetailItems , setProjectDetailItems ] = useState<ProjectDetailItems>({id : 0 , open : false});
+  const [ projectDetailItems , setProjectDetailItems ] = useState<ProjectDetailItems>({ selectedProject : null , open : false});
 
   useEffect(() => {
     const el = boxRef.current;
@@ -42,7 +42,7 @@ const Projects = () => {
       >
         {projects.map(item => ( 
           <Grow key={item.id} in={visible} {...(visible ? { timeout: item.id * 1000 } : {})} >
-            <Box className="project-box" onClick={() => setProjectDetailItems({ id : item.id , open : true})} >
+            <Box className="project-box" onClick={() => setProjectDetailItems({ selectedProject : item , open : true})} >
               <Image  className="project-img"  alt={item.name} src={item.url} width={1000} height={1000} style={{  width : "100%" , height : "100%" , borderRadius : "5px" , transition : "all 0.4s" }} />
             </Box>
           </Grow>
@@ -59,22 +59,46 @@ interface ProjectType {
   id : number
   name : string
   url : string
+  headingUrl : string;
+  headingTypo : string;
+  headingDetail : HeadingDetail[]
+  liveDemo : string;
+  codeSource : string;
+  bodyDetail : BodyDetailType[];
 }
 
 const projects : ProjectType[] = [
   {
     id : 0 ,
-    name : "Moon Maocai",
-    url : "/moon-maocai.png"
+    name : "Mala Maocai",
+    url : "/moon-maocai.png",
+    headingUrl : "/moon-maocai.png",
+    headingTypo : "Bridging the gap between hungry customers and efficient kitchen management.",
+    headingDetail : malaMaocaiHeadingDetails,
+    codeSource : "https://github.com/sithu-developer/maocai.git",
+    liveDemo : "https://moon-maocai.vercel.app",
+    bodyDetail : []
   },
   {
     id : 1 ,
     name : "Happy Chatting",
-    url : "/happy-chatting.png"
+    url : "/happy-chatting.png",
+    headingUrl : "/hc-phone.png",
+    headingTypo : "A straightforward messaging platform that strips away the noise, focusing purely on what matters: the conversation.",
+    headingDetail : digitalVotingHeadingDetails,
+    codeSource : "https://github.com/sithu-developer/chatting.git",
+    liveDemo : "https://chatting-rust.vercel.app",
+    bodyDetail : happyChattingDetails
   },
   {
     id : 2 ,
     name : "Digital Voting",
-    url : "/digital-voting.png"
+    url : "/digital-voting.png",
+    headingUrl : "/digital-voting-sign-in.png",
+    headingTypo : "Developed a secure and user-friendly digital voting application for university events, such as a \"Fresher Welcome\" celebration.",
+    headingDetail : happyChattingHeadingDetails,
+    codeSource : "https://github.com/sithu-developer/Digital-Voting.git",
+    liveDemo : "https://digital-voting.vercel.app",
+    bodyDetail : digitalVotingDetails
   }
 ]
